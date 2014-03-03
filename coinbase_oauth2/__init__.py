00ecc55bd618e4f016b59af25cdd6b568505b7fb
Paul Mestemaker <Paul.Mestemaker@gmail.com>
2014-03-03 02:37:58 -0800

    Updated OAuth2 example * Fixed critical bugs (URIs were non-functional) * Reduced default scope to 'transactions balance' instead of 'all' * Added helpful messages for common problems that new developers will run into (secrets file not existing, no notification when web server was starting) * Added an off-by-default way to disable certificate validation

--- coinbase_oauth2/__init__.py
+++ coinbase_oauth2/__init__.py
@@ -1,4 +1,4 @@
-__author__ = 'gsibble'
+__author__ = 'gsibble, PaulMest'
 
 from flask import Flask, render_template, request, make_response
 
@@ -8,17 +8,34 @@ import httplib2
 APP = Flask(__name__)
 APP.debug = True
 
+host='localhost'
+port=5000
+
+
 import logging
 logging.basicConfig()
 
-from secrets import CALLBACK_URL, CLIENT_ID, CLIENT_SECRET
+try:       
+	from secrets import CALLBACK_URL, CLIENT_ID, CLIENT_SECRET
+except:
+    print '''You need to create a secrets.py file in location ./coinbase_oauth2/secrets.py. It should look something like this:
+__author__ = "Paul Mestemaker"
+CALLBACK_URL="http://{host}:{port}/consumer_auth"              
+CLIENT_ID="[Get this from Coinbase]"
+CLIENT_SECRET="[Get this from Coinbase]"'''.format(host=host, port=port)
+    exit()
 
-coinbase_client = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, 'all', redirect_uri='http://www.paywithair.com/consumer_auth', auth_uri='https://www.coinbase.com/oauth/authorize', token_uri='https://www.coinbase.com/oauth/token')
+from coinbase.config import COINBASE_AUTH_URI, COINBASE_TOKEN_URI
+
+# SCOPE = 'all'
+SCOPE = 'transactions balance'
+
+coinbase_client = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, SCOPE, redirect_uri=CALLBACK_URL, auth_uri=COINBASE_AUTH_URI, token_uri=COINBASE_TOKEN_URI)
 
 @APP.route('/')
 def register_me():
 
-    auth_url = coinbase_client.step1_get_authorize_url()
+    auth_url = coinbase_client.step1_get_authorize_url()    
 
     return render_template('register.jinja2', auth_url=auth_url)
 
@@ -30,10 +47,16 @@ def receive_token():
     print oauth_code
 
     http = httplib2.Http(ca_certs='/etc/ssl/certs/ca-certificates.crt')
+    # If you have difficulty with certificate validation, you can use this less secure workaround to test the rest of your code:
+    # http = httplib2.Http(disable_ssl_certificate_validation=True) # Note: This is fine for development/testing, but not for production
 
     token = coinbase_client.step2_exchange(oauth_code, http=http)
 
-    return make_response(token.to_json())
+    directions = "Use this JSON result in TEMP_CREDENTIALS (e.g. ./coinbase/config.py or ./example.py):\n{json}".format(json=token.to_json())
+    print directions
+
+    return make_response(directions.replace('\n', '<br/>'))
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=80)
+    print "Starting web server... Visit: http://{host}:{port}".format(host=host, port=port)
+    APP.run(host=host, port=port)
